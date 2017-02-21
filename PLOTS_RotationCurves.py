bb60b2e697c5003ff6261b17f9d4d380ee45b6ea
HiggsBoson3310 <miguel.alarcon3310@gmail.com>
2017-02-21 07:31:59 -0500

    Add files via upload

--- PLOTS_RotationCurves.py
+++ PLOTS_RotationCurves.py
@@ -1,4 +1,4 @@
-#importamos los paquetes necesarios para desarrollar el problema
+#Importamos los paquetes necesarios para desarrollar el problema
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import *
@@ -9,6 +9,7 @@ lists = data.readlines()
 datos=[]
 for i in range(len(lists)):
     datos.append(lists[i].split())
+#Definimos las listas que van a contener los datos que queremos
 ARad = []
 PRad = []
 Vgas = []
@@ -16,6 +17,7 @@ Vdisk = []
 VBul = []
 Vel = []
 e_Vel = []
+#Organizamos los datos en dichas listas.
 for columna in datos:
     ARad.append(float(columna[1]))
     PRad.append(float(columna[2]))
@@ -24,13 +26,14 @@ for columna in datos:
     VBul.append(float(columna[5]))
     Vel.append(float(columna[6]))
     e_Vel.append(float(columna[7]))
-#Con las listas correspondientes a cada uno de los datos podemos realizar las graficasque queremos obtener
+#Con las listas correspondientes a cada uno de los datos podemos realizar las graficas que queremos obtener
 y = np.array(Vgas)+np.array(Vdisk)+np.array(VBul)
 plt.plot(PRad, Vel, c='k', label='Velocidad Medida')
-plt.plot(PRad, y, c='g', label='Suma de velocidad de bulbo, gas y disco')
+plt.plot(PRad, y, c='g', label='Velocidad esperada (gas+disco+bulbo)')
 plt.xlabel('Radio (kpc)')
 plt.ylabel('Velocidad (km/s)')
 plt.title('Velocidad de Roatcion en F571-8')
+plt.legend(loc=0)
 #Finalmente guardamos la grafica en un archivo pdf
 plt.savefig('RotationCurvePlot.pdf')
 
